4c6fa6f76af846180d8f6a949da57da6bd1a2427
Annie Truex <artruex@gmail.com>
2019-06-04 21:10:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,29 @@
-class Artist 
-  
-  attr_accessor :name 
-  
-  def initialize (name)
-    @name = name 
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
     @songs = []
   end
-  
+
   def add_song(song)
-    name.song = self
+    @songs << song
+    song.artist = self
+    @@song_count += 1
   end
-  
+
   def add_song_by_name(name)
     song = Song.new(name)
-    name.song = self
+    @songs << song
+    song.artist = self
+    @@song_count += 1
   end
-  
-  def songs
-    @songs
-  end
-  
+
   def self.song_count
-    Song.all.count
+    @@song_count
   end
-  
+
 end
